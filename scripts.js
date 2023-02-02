48c8a556ae40495db3c96beb2fb9ea2a6d4c68e8
PercolatingFish <robfarisvideography@gmail.com>
2023-02-02 14:40:42 -0500

    Add sketch functionality

--- scripts.js
+++ scripts.js
@@ -2,24 +2,44 @@ const container = document.querySelector('.container');
 let squares = 16;
 
 function gridMaker(squares) {
+    container.innerHTML = '';
     for (let i = 0; i < squares ** 2; i++) {
         const div = document.createElement('div');
         div.className = "gridPart";
         div.style.width = (960 / squares) + 'px';
         div.style.height = (960 / squares) + 'px';
+        div.style.filter = "brightness(99%)"
         
         container.appendChild(div);
+        div.addEventListener('mouseover', turnBlack);
     }
 }
 
-// send the user a popup asking for number of squares per side for the new grid
-
 function popup() {
     let squares = prompt("How many squares do you want?");
+    if (squares > 100) {
+        squares = prompt("Try again, less than 100 please");
+    }
     gridMaker(squares)
 }
 
-// generate a new grid in the same total space
+function turnBlack() {
+    this.className = "gridSelected";
+    this.style.backgroundColor = getRandomColor()
+    let brightness = (this.style.filter).substring(11,13);
+    brightness -= 10;
+    this.style.filter = `brightness(${brightness}%)`;
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 
 const popupBtn = document.querySelector('.popupBtn');
 popupBtn.addEventListener("click", popup);
